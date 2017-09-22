c3f53e169883f3125375d793042c12344ef2174d
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-09-21 22:03:21 -0700

    Sticking to using native function

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
-/* eslint-disable no-magic-numbers */
-
-import reduceValues from "@unction/reducevalues"
 import equals from "@unction/equals"
-import ifThenElse from "@unction/ifthenelse"
-import append from "@unction/append"
-import always from "@unction/always"
-import flip from "@unction/flip"
 
 export default function split (delimiter: string): Function {
   const equalsDelimiter = equals(delimiter)
 
-  return reduceValues(
-    (sections: Array<string>): Function =>
-      ifThenElse(
-        equalsDelimiter
-      )(
-        always(sections)
-      )(
-        flip(append)(sections)
-      )
-    )(
-      []
-    )
+  return function splitDelimiter (text: string): Array<StringType> {
+    return text.split(delimiter)
+  }
 }
