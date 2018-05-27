53551e6766243f173154bf63a24a2fd0df6d7ed6
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 18:41:46 -0700

    Importing cool new types

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-export default function split (delimiter: StringType): Function {
-  return function splitDelimiter (text: StringType | Regexp): Array<StringType> {
+import type {StringType} from "types"
+import type {Regexp} from "types"
+import type {UnaryFunctionType} from "types"
+
+export default function split (delimiter: StringType | Regexp): UnaryFunctionType {
+  return function splitDelimiter (text: StringType): ArrayType<StringType> {
     return text.split(delimiter)
   }
 }
